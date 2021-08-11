32d0483bdb2068fc51eaf9225470737ec2073880
chicchi <coi_mp@yahoo.co.jp>
2021-08-12 01:09:05 +0900

    mac用の対応を追加

--- build_mac.sh
+++ build_mac.sh
@@ -1,6 +1,7 @@
 yacc_result=`goyacc -o compiler/parser.go compiler/parser.go.y`
 if [ $yacc_result != ""]; then
-    go run . testscript/$1.ks bin/$1.ksobj
+    go build -o ks2compiler app.go
+    ./ks2compiler testscript/$1.ks bin/$1.ksobj
     exit 0
 fi
 exit 1
